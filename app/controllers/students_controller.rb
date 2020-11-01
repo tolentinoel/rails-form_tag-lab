49a690d603c23cbc0bf7c5b3f5411cef3f86a0d2
tolentinoel <rckrleyn@gmail.com>
2020-11-01 00:00:49 -0700

    commit tree d5f428698e7aa42dfb626f5f65fdc7aebbbd0e0d

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,36 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+    @student = Student.new(s_params)
+
+    if @student.valid?
+      @student.save
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def s_params
+    params.require(:student).permit(:first_name, :last_name)
+  end
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
